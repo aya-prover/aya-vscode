ecff8af11b78f8cffdf6e6a70c7b75c882371093
imkiva <imkiva@islovely.icu>
2021-07-13 00:57:49 +0800

    TYPE: support decoration

--- src/compute-type.ts
+++ src/compute-type.ts
@@ -7,19 +7,22 @@ export interface ComputeTypeResult {
   range: vscode.Range,
 }
 
+const deco = {
+  borderStyle: 'solid',
+  borderColor: '#66f'
+};
+
+const decoCurrent = vscode.window.createTextEditorDecorationType(
+  Object.assign({}, deco, { borderWidth: '0px 0px 1px 0px' }));
+
 export function applyComputedType(editor: vscode.TextEditor, r: ComputeTypeResult): any {
   console.log(JSON.stringify(r));
   if (editor.document.uri.toString() !== r.uri) return;
-
-  const deco = {
-    borderStyle: 'solid',
-    borderColor: '#66f'
-  };
-  const decoCurrent = vscode.window.createTextEditorDecorationType(
-    Object.assign({}, deco, { borderWidth: '0px 0px 1px 0px' }));
 
   editor.setDecorations(decoCurrent, [{
     range: r.range,
     hoverMessage: r.computedType
   }]);
+
+  editor.selection = new vscode.Selection(r.range.start, r.range.end);
 }
